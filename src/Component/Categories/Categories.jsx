08c27b632ed645cccd484e14e6180922558473d7
gemy3731 <mohamedgamalz3731@gmail.com>
2024-08-31 02:08:16 +0300

    Add cache to categories page by useing react-query

--- src/Component/Categories/Categories.jsx
+++ src/Component/Categories/Categories.jsx
@@ -1,33 +1,39 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import Loader from "../Loader/Loader";
 
 
 export default function Categories() {
-  const [categories, setCategories] = useState([]);
-      useEffect(() => {
-        getAllCategories();
-      }, []);
+
       function getAllCategories() {
-        axios
-          .get("https://ecommerce.routemisr.com/api/v1/categories")
-          .then(({ data }) => {
-            setCategories(data.data);
-            console.log(data);
-          })
-          .catch((err) => console.log(err));
+      return axios.get("https://ecommerce.routemisr.com/api/v1/categories")
       }
+      const {data,isLoading} = useQuery({
+        queryKey:["allCategories"],
+        queryFn:getAllCategories,
+        staleTime:1000*60*10,
+        select:(data)=>data?.data?.data
+      });
+      console.log(data)
   return (
-    
+    <>
+    {isLoading?<Loader />:(
     <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-4 gap-4 my-6">
-      {categories.map((category)=><div key={category._id} className="product rounded-lg overflow-hidden">
+      {console.log(data)}
+      {data.map((category)=><div key={category._id} className="product rounded-lg overflow-hidden">
       <Link to={`/specificCategory/${category._id}`}>
         <img src={category.image} className="w-full h-[450px]" alt={category.name} />
         <div className="p-2 text-center">
           <h2 className="text-[#4fa74f] text-2xl ">{category.name}</h2>
         </div>
       </Link>
+      {console.log(data)}
+      
     </div>)}
     </div>
+    )}
+    </>
   );
 }
