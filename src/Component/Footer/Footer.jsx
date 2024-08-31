dcb83fd03bbf4022236d2ce1288e81babcce8dda
gemy3731 <mohamedgamalz3731@gmail.com>
2024-08-31 03:38:41 +0300

    Add some edits to Footer and NavBar

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import freshCart from '../../assets/images/freshcart-logo.svg'
 
 export default function Footer() {
   return (
-    <div className='bg-white border-t border-gray-300 p-5 text-center'>Footer</div>
+    <div className='bg-white border-gray-300 p-5 text-center'>
+      <div className="container w-[80%] mx-auto">
+        <div className='flex flex-col justify-center lg:flex-row lg:justify-between items-center'>
+          <div className='w-2/6 lg:w-1/6 lg:me-auto'>
+            <img className='w-full' src={freshCart} alt="logo" />
+          </div>
+      <div className=" w-full lg:w-auto" id="navbar-default">
+      <ul className="font-medium flex flex-col items-center justify-center text-center p-4 lg:p-0 mt-4 rounded-lg  lg:flex-row lg:space-x-8 rtl:space-x-reverse lg:mt-0 text-xl  text-black">
+        <li className='space-x-4'>
+          <a href="https://www.instagram.com/" target='_blank'>
+          <i className='fa-brands fa-instagram'></i>
+          </a>
+          <a href="https://www.facebook.com/" target='_blank'>
+          <i className='fa-brands fa-facebook'></i>
+          </a>
+          <a href="https://www.tiktok.com/" target='_blank'>
+          <i className='fa-brands fa-tiktok'></i>
+          </a>
+          <a href="https://www.twitter.com/" target='_blank'>
+          <i className='fa-brands fa-twitter'></i>
+          </a>
+          <a href="https://www.linkedin.com/" target='_blank'>
+          <i className='fa-brands fa-linkedin'></i>
+          </a>
+          <a href="https://www.youtube.com/" target='_blank'>
+          <i className='fa-brands fa-youtube'></i>
+          </a>
+        </li>
+        </ul>
+        </div>
+        </div>
+        <p>Copy Right 2018 © By Daniels All Rights Reserved</p>
+      </div>
+    </div>
   )
 }
--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -26,12 +26,13 @@ export default function NavBar() {
   return (
 <nav className="bg-white shadow-xl lg:fixed top-0 left-0 right-0 z-40">
   <div className="container mx-auto">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    
-    <div className=" w-full flex flex-wrap flex-col lg:flex-row items-center lg:w-auto" id="navbar-default">
-    
-        <img src={freshCart} className="h-8 me-3" alt="freshCart" />
-      {token&&<ul className="font-medium flex flex-col text-xl p-4 lg:p-0 mt-4 text-center  rounded-lg  lg:flex-row lg:space-x-8 rtl:space-x-reverse lg:mt-0  text-black">
+  <div className="max-w-screen-2xl mx-auto py-4">
+    <div className="flex flex-wrap flex-col lg:flex-row items-center lg:justify-around " id="navbar-default">
+      <div>
+        <img src={freshCart} className="h-8" alt="freshCart" />
+      </div>
+      <div>
+      {token&&<ul className="font-medium flex flex-col text-xl p-4 lg:p-0 mt-4 text-center rounded-lg lg:flex-row lg:space-x-8 rtl:space-x-reverse lg:mt-0  text-black">
         <li>
           <NavLink to="home" className="block py-2 px-3 lg:p-0  " aria-current="page">Home</NavLink>
         </li>
@@ -56,30 +57,9 @@ export default function NavBar() {
           <NavLink to="brands" className="block py-2 px-3 rounded lg:p-0">Brands</NavLink>
         </li>
       </ul> }
-      
-    </div>
-    <div className=" w-full  lg:w-auto" id="navbar-default">
+      </div>
+    <div className=" w-full lg:w-auto" id="navbar-default">
       <ul className="font-medium flex flex-col items-center justify-center text-center p-4 lg:p-0 mt-4 rounded-lg  lg:flex-row lg:space-x-8 rtl:space-x-reverse lg:mt-0 text-xl  text-black">
-        <li className='space-x-4'>
-          <a href="https://www.instagram.com/" target='_blank'>
-          <i className='fa-brands fa-instagram'></i>
-          </a>
-          <a href="https://www.facebook.com/" target='_blank'>
-          <i className='fa-brands fa-facebook'></i>
-          </a>
-          <a href="https://www.tiktok.com/" target='_blank'>
-          <i className='fa-brands fa-tiktok'></i>
-          </a>
-          <a href="https://www.twitter.com/" target='_blank'>
-          <i className='fa-brands fa-twitter'></i>
-          </a>
-          <a href="https://www.linkedin.com/" target='_blank'>
-          <i className='fa-brands fa-linkedin'></i>
-          </a>
-          <a href="https://www.youtube.com/" target='_blank'>
-          <i className='fa-brands fa-youtube'></i>
-          </a>
-        </li>
         {token?<>
           <li>
           <button onClick={logOut} className="block rounded-lg py-2 lg:py-1 my-2 lg:my-0 px-3 bg-black text-white" >Log Out</button>
@@ -92,12 +72,10 @@ export default function NavBar() {
           <Link to="register" className="block py-2 lg:py-0 px-3 my-2 lg:my-0 rounded-lg  bg-black text-white hover:text-white" >Register</Link>
         </li>
         </>}
-        
-        
       </ul>
     </div>
+    </div>
   </div>
-
   </div>
 </nav>
 
