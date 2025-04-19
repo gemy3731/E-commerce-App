2451fdb27f39957fa245d1b3b969de1a1b64bb6b
Mohamed Gamal Hafez <mohamedgamalz3731@gmail.com>
2025-04-19 19:02:56 +0200

    Update Products.jsx

--- src/Component/Products/Products.jsx
+++ src/Component/Products/Products.jsx
@@ -141,6 +141,7 @@ export default function Products() {
             Search
           </label>
         </div>
+          <h2 className="text-5xl font-bold text-black text-center mb-16">Products</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
             {filteredData.map((product) => {
               return (
